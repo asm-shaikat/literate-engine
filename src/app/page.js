6ac76545292e340978a28141ade0794ed5d88e85
shadat.shaikat <shadat.shaikat@annanovas.net>
2025-12-09 11:41:36 +0600

    initial project code

---
+++ src/app/page.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="p-6">
+        <p>Aliqua minim proident consequat nisi ea cupidatat nostrud proident est duis nostrud nisi. Anim labore proident consequat consequat eu irure non eiusmod adipisicing. Irure non amet velit tempor. In nulla aute ad velit aliqua irure ipsum est laboris. Fugiat aliquip commodo deserunt esse ullamco voluptate. Qui occaecat laborum laboris occaecat sunt elit incididunt anim.
+
+          Laborum velit sunt et reprehenderit. Culpa fugiat cupidatat dolor eiusmod dolor duis anim irure nisi. Enim aute reprehenderit elit magna est dolore consectetur nostrud deserunt quis officia aute do. Ullamco minim veniam labore dolore dolor non est deserunt Lorem eu.
+
+          Adipisicing aliquip pariatur do voluptate esse ipsum proident sit id magna mollit reprehenderit anim. Nostrud occaecat duis Lorem anim ut tempor duis sunt commodo ut nulla. Esse minim enim do consequat ea aliqua amet labore culpa quis sint. Culpa adipisicing ipsum consequat aute.
+
+          Enim voluptate ut commodo do eiusmod ad qui eu fugiat Lorem aute qui consectetur ea. Est nostrud non ea tempor incididunt sit cillum nostrud veniam. In irure elit incididunt ea duis officia. Commodo tempor anim anim veniam aliqua aliquip mollit quis sunt eiusmod aliqua laborum. Labore ipsum magna elit sit quis duis in excepteur ipsum quis excepteur et elit deserunt. Anim id cupidatat fugiat dolore duis aute incididunt elit labore. Ullamco culpa labore nisi elit.
+
+          Elit reprehenderit quis velit reprehenderit exercitation. Ipsum consectetur ad sint aliqua commodo fugiat veniam enim aliquip aliquip elit velit et sit. Qui duis sunt veniam voluptate.
+
+          Cillum velit nulla nisi proident mollit Lorem duis do. Et ullamco aliquip ea commodo nulla nisi quis. Officia nisi ullamco commodo exercitation incididunt. Quis esse eiusmod excepteur nostrud elit in aute ut ex ad nostrud pariatur cupidatat dolore.
+
+          Reprehenderit est officia mollit amet consequat reprehenderit dolor. Laboris ea cillum sint ullamco aliquip consectetur. Deserunt nisi esse voluptate qui sit est adipisicing fugiat. Ullamco deserunt et magna ea et ex elit proident voluptate ut labore eu. Anim nostrud nisi in irure excepteur incididunt culpa sunt.
+
+          Aliqua consequat culpa id do Lorem do dolor esse. Consequat reprehenderit esse nisi velit commodo eu elit. Id nisi ad pariatur ea tempor nisi commodo id. Cillum commodo cillum cillum aliquip nostrud cupidatat est et voluptate. Incididunt laborum consequat laborum aliqua anim ea labore.
+
+          Pariatur amet nostrud voluptate duis do incididunt adipisicing irure laborum velit consequat nulla. Velit voluptate aliquip reprehenderit quis laboris. Veniam qui non amet aliquip.
+
+          Labore incididunt sit excepteur cupidatat laborum dolor velit. Magna excepteur adipisicing elit elit officia culpa ex dolor minim deserunt ut elit. Tempor commodo velit incididunt excepteur esse ad do commodo aute reprehenderit. Officia ullamco exercitation eiusmod anim est id pariatur esse do esse aliqua. Enim occaecat labore excepteur nostrud nulla et occaecat ut. Aliquip consequat nostrud excepteur proident ullamco irure proident esse deserunt.
+        </p>
+      </div>
+    </div>
+  );
+}
